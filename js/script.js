fe2bbe52edb0d0061578edb53c229c120c8c3879
Sadjin <sadjinandrej@gmail.com>
2018-09-05 12:12:18 +0300

    Правки по замечаниям

--- js/script.js
+++ js/script.js
@@ -3,23 +3,24 @@ var wizardWidth = 70;
 var fireballSize = 22;
 var fireballSpeedDownWind = 5;
 var fireballSpeedUpWind = 2;
+var ratioHeightToWidthWizard = 1.337;
 
 
 var getFireballSpeed = function (left) {
   if (left) {
     return fireballSpeedDownWind;
-  } 
+  }; 
   return fireballSpeedUpWind;
-}
+};
 
 var getWizardHeight = function () {
-  return 1.337 * wizardWidth;
-}
+  return ratioHeightToWidthWizard * wizardWidth; //
+};
 
 var getWizardX = function (width) {
-  return (width * .5) - (wizardWidth / 2);
-}
+  return (width - wizardWidth) / 2;
+};
 
 var getWizardY = function (height) {  
-  return height * 0.333333;
-}
+  return height / 3;
+};
